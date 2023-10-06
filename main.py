138fab19e3b0865c833ebb9a7c9e5bf434f003a9
Edward Carter <146713869+edward-fc@users.noreply.github.com>
2023-10-06 14:56:43 +0000

    new code for main

--- main.py
+++ main.py
@@ -4,7 +4,30 @@ filename = "amysrecords.csv"
 file = open(filename, 'r')
 f = file.read() 
 l = []
-text = ""
+Rank = []
+Name = []
+Group = []
+Year = []
+memory = ""
+counter = 0
+circle = 1
 for line in f:
-    text += line
-    l.append(line)
+    if line != ",":
+        memory += line
+    else:
+        if circle == 1:
+            Rank.append(memory)   
+        if circle == 2:
+            Name.append(memory)
+        if circle == 3:
+            Group.append(memory)
+        if circle == 4:
+            Year.append(memory)
+            counter+=1
+        if counter==4:
+            counter=0
+        circle=0
+        memory =""
+        circle+=1
+
+print(Rank)
