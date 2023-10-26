5f14589e768f2c7073b9c914755501d283164b42
Edward Carter <146713869+edward-fc@users.noreply.github.com>
2023-10-26 16:47:33 +0000

    try my best to formating it

--- Task_1_Assignement/StepsTask1.c
+++ Task_1_Assignement/StepsTask1.c
@@ -45,13 +45,25 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
+    //Initialise the Fitness Data
     FITNESS_DATA DATA;
     FITNESS_DATA MEMORIE[3];
+    //Open the file
     FILE *file = fopen("FitnessData_2023.csv","r");
-    int buffer_size = 100,count=0;
+    //Initiation of variables
+    int buffer_size = 100,count=0,len=0;
     char line_buffer[buffer_size],delimiter = ',';
+    //Take line by line until the end
     while(fgets(line_buffer, buffer_size, file) != NULL){
+        //Use the function provided
         tokeniseRecord(line_buffer,&delimiter,DATA.date,DATA.time,DATA.steps);
+        //grab the length of Data Steps
+        len = strlen(DATA.steps)-1;
+        //See if the end of the string still has a format
+        if (*DATA.steps && DATA.steps[len] == '\n') {
+            DATA.steps[len] = '\0';
+            }
+        //Copy the 3 first lines to print them later
         if ( count < 3) {
             strcpy(MEMORIE[count].date, DATA.date);
             strcpy(MEMORIE[count].time, DATA.time);
@@ -59,12 +71,14 @@ int main() {
             }
         count++;
     }
+    //Print the number of records in the file
     printf("Number of records in file: %d\n",count);
+    //Print the Three first lines
     for (int i = 0; i < 3; i++)
     {
-        printf("%s/%s/%s",MEMORIE[i].date,MEMORIE[i].time,MEMORIE[i].steps);
+        printf("%s/%s/%s\n",MEMORIE[i].date,MEMORIE[i].time,MEMORIE[i].steps);
     }
-    
+    //Close the file
     fclose(file);
     return 0;
 }
