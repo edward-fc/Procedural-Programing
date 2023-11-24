19fc7cac71980e472ebb14b7496118a01d247d12
BuildTools <unconfigured@null.spigotmc.org>
2023-11-24 11:23:00 +0000

    fixed some probelms and renamed function

--- Task_2_Assignement/FitnessDataStruct.h
+++ Task_2_Assignement/FitnessDataStruct.h
@@ -1,6 +1,6 @@
 
-#ifndef FITNESS_DATA_STRUCT_H
-#define FITNESS_DATA_STRUCT_H
+// #ifndef FITNESS_DATA_STRUCT_H
+// #define FITNESS_DATA_STRUCT_H
 
 #include <stdio.h>
 
@@ -11,6 +11,162 @@ typedef struct {
 	char steps[8];
 } FITNESS_DATA;
 
-// Helper function prototypes
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
 
-#endif // FITNESS_DATA_STRUCT_H
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    delimiter=",";
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {
+        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
+
+/**
+ * @brief Opens the file in the correct mode
+ *
+ * @param filename the name of the file to open
+ * @param mode the mode (r/w/a/r+/w+/a+)
+ * @return FILE* The file object to store the opened file in.
+ */
+FILE *open_file(char *filename, char *mode)
+{
+    FILE *input = fopen(filename, mode);
+    return input;
+}
+
+/**
+ * @brief Reads the data from the input file into an array of structs
+ *
+ * @param inputFile the open file object
+ * @param dataArray the array of readings
+ * @return int Returns the number of readings from the file
+ */
+int read_file(FILE *inputFile, FITNESS_DATA *DATA,FITNESS_DATA *MEMORY)
+{
+	//Initiation of variables
+    int buffer_size = 100,len=0,function_count=0;
+    
+    char line_buffer[buffer_size],delimiter = ',';
+
+    while(fgets(line_buffer, buffer_size, inputFile) != NULL){
+        //Use the function provided
+        tokeniseRecord(line_buffer,&delimiter,DATA->date,DATA->time,DATA->steps);
+        //grab the length of Data Steps
+        len = strlen(DATA->steps)-1;
+        //See if the end of the string still has a format
+        if (DATA->steps && DATA->steps[len] == '\n') {
+            DATA->steps[len] = '\0';
+            }
+        //Copy the 3 first lines to print them later
+        
+        strcpy(MEMORY[function_count].date, DATA->date);
+        strcpy(MEMORY[function_count].time, DATA->time);
+        strcpy(MEMORY[function_count].steps, DATA->steps);
+            
+        function_count++;
+    }
+	return function_count;
+}
+
+/**
+ * @brief Checks that there was data for each part of each reading in the file
+ *
+ * @param DATA The array of data from the file
+ * @param Count The number of readings in the array
+ * @return int Return 0 if there are no errors, 1 if you find an error.
+ */
+int data_checker(FITNESS_DATA *DATA, int count)
+{
+	if (count == 0){
+		return 1;
+	}
+    for(int i = 0; i< count; i++){
+        if (DATA[i].steps == NULL){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+/**
+ * @brief Calculates and returns the mean of the readings in the array
+ * 
+ * @param dataArray The array of data from the file
+ * @param numReadings The number of readings in the array
+ * @return float The mean of the readings.
+ */
+int find_mean(FITNESS_DATA* dataArray, int numReadings)
+{
+    int mean = 0;
+    for (int i=0; i<numReadings;i++){
+        mean += atoi(dataArray[i].steps);
+    }
+    mean /= numReadings;
+    return mean;
+}
+
+/**
+ * @brief Finds and returns the highest blood iron reading
+ * 
+ * @param dataArray The array of data from the file
+ * @param numReadings The number of readings in the array
+ * @return float The highest blood iron reading
+ */
+int find_highest(FITNESS_DATA* DATA, int count)
+{
+    int maximun = atoi(DATA[0].steps);
+	int rank;
+    for(int i = 1; i < count;i++){
+        if (maximun < atoi(DATA[i].steps)){
+            maximun = atoi(DATA[i].steps);
+			rank=i;
+        }
+    }
+    printf("Largest steps: %s %s\n",DATA[rank].time,DATA[rank].date);
+    return maximun;
+}
+
+/**
+ * @brief Finds and returns the lowest blood iron reading
+ * 
+ * @param dataArray The array of data from the file
+ * @param numReadings The number of readings in the array
+ * @return float The lowest blood iron reading
+ */
+int find_lowest(FITNESS_DATA* DATA, int count)
+{
+	int minimun = atoi(DATA[0].steps);
+	int rank;
+    for(int i = 1; i < count;i++){
+        if (minimun > atoi(DATA[i].steps)){
+            minimun = atoi(DATA[i].steps);
+			rank=i;
+        }
+    }
+    printf("Fewest steps: %s %s\n",DATA[rank].time,DATA[rank].date);
+    return minimun;
+}
+
+
+// #endif // FITNESS_DATA_STRUCT_H
--- Task_2_Assignement/StepCounter_Final.c
+++ Task_2_Assignement/StepCounter_Final.c
@@ -9,77 +9,35 @@
 // Global variables for filename and FITNESS_DATA array
 
 
-// This is your helper function. Do not change it in any way.
-// Inputs: character array representing a row; the delimiter character
-// Ouputs: date character array; time character array; steps character array
-void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
-    // Create a copy of the input string as strtok modifies the string
-    char *inputCopy = strdup(input);
-    delimiter=",";
-    // Tokenize the copied string
-    char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {
-        strcpy(date, token);
-    }
-    
-    token = strtok(NULL, delimiter);
-    if (token != NULL) {
-        strcpy(time, token);
-    }
-    
-    token = strtok(NULL, delimiter);
-    if (token != NULL) {
-        strcpy(steps, token);
-    }
-    
-    // Free the duplicated string
-    free(inputCopy);
-
-                    }
 
 
-void A(FITNESS_DATA MEMORY[],char *filename,char *count){
+
+void fetch_store_data(FITNESS_DATA MEMORY[],char *filename,char *count){
     //Initialise the Fitness Data
     FITNESS_DATA DATA;
     //Open the file
-    FILE *file = fopen(filename,"r");
-    //Initiation of variables
-    int buffer_size = 100,len=0,function_count=0;
-    
-    char line_buffer[buffer_size],delimiter = ',';
+    FILE *file = open_file(filename,"r");
+    printf("%s\n",file);
+    if (file)
+    {       
+    int function_count;
     //Take line by line until the end
-    while(fgets(line_buffer, buffer_size, file) != NULL){
-        //Use the function provided
-        tokeniseRecord(line_buffer,&delimiter,DATA.date,DATA.time,DATA.steps);
-        //grab the length of Data Steps
-        len = strlen(DATA.steps)-1;
-        //See if the end of the string still has a format
-        if (DATA.steps && DATA.steps[len] == '\n') {
-            DATA.steps[len] = '\0';
-            }
-        //Copy the 3 first lines to print them later
-        
-        strcpy(MEMORY[function_count].date, DATA.date);
-        strcpy(MEMORY[function_count].time, DATA.time);
-        strcpy(MEMORY[function_count].steps, DATA.steps);
-            
-        function_count++;
-    }
+    function_count = read_file(file,&DATA,MEMORY);
     char token[6];
     sprintf(token, "%d", function_count);
     strcpy(count,token);
     //Close the file
     fclose(file);
+    }
 }
-char get_user_answer(){
-    char token;
-    scanf("%[AaBbCcDdEeFfQq]", &token);
-    return token;
-}
+// char get_user_answer(){
+//     char token;
+//     scanf("%[AaBbCcDdEeFfQq]", &token);
+//     return token;
+// }
 void MENU(FITNESS_DATA DATA[]){
     int QUIT=1,rank=0,mean=0,longest=0,new_longest=0, new_run=1,rank_start=0,rank_end=0;
-    char input, filename,token [50],count[6],minimun[6],maximun[6];
+    char input, filename,token [50],count[6],minimun[6],maximun[6],fail=0;
     
 
     while(QUIT){
@@ -96,10 +54,17 @@ void MENU(FITNESS_DATA DATA[]){
         switch (input){
             case 'A':
             case 'a':
+            
             printf("Input filename: ");
             scanf("%s",token);
-            A(DATA,token,count);
-            printf("File successfully loaded.\n");
+            fetch_store_data(DATA,token,count);
+            fail = data_checker(DATA,atoi(count));
+            if (fail){
+                printf("Could not find or open the file.\n");
+            }
+            else{
+                printf("File successfully loaded.\n");
+            }
             break;
             
             case 'B':
@@ -109,35 +74,18 @@ void MENU(FITNESS_DATA DATA[]){
 
             case 'C':
             case 'c':
-            strcpy(minimun, DATA[0].steps);
-            for(int i=0; i<atoi(count);i++){
-                if (atoi(minimun) < atoi(DATA[i].steps)){
-                    strcpy(minimun, DATA[i].steps);
-                    rank=i;
-                }
-            }
-            printf("Fewest steps: %s %s\n",DATA[rank].time,DATA[rank].date);
+            find_lowest(DATA,atoi(count));
             break;
 
             case 'D': 
             case 'd':
-            strcpy(maximun, DATA[0].steps);
-            for(int i=0; i<atoi(count);i++){
-                if (atoi(maximun) < atoi(DATA[i].steps)){
-                    strcpy(maximun, DATA[i].steps);
-                    rank=i;
-                }
-            }
-            printf("Largest steps: %s %s\n",DATA[rank].time,DATA[rank].date);
+            find_highest(DATA,atoi(count));
             break;
 
             case 'E':
             case 'e':
             
-            for (int i = 0; i<atoi(count); i++){
-                mean += atoi(DATA[i].steps);
-            }
-            mean=mean/atoi(count);
+            mean = find_mean(DATA,atoi(count));
             printf("Mean step count: %i\n",mean);
             break;
 
