a7f931e2f62bab1c4136ec30622d25fe0e5fd85a
Edward Carter <146713869+edward-fc@users.noreply.github.com>
2024-02-12 15:41:05 +0000

    started the assignement for programming project

--- Maze game/MazeDataStruct.h
+++ Maze game/MazeDataStruct.h
@@ -16,7 +16,7 @@ typedef struct {
 // Ouputs: date character array; time character array; steps character array
 
 void tokeniseRecord(const char *input, const char *delimiter,
-                    char *row[100]) {
+                    char *row) {
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
     delimiter=",";
@@ -26,11 +26,11 @@ void tokeniseRecord(const char *input, const char *delimiter,
     while (token != NULL){
         
         if (token != NULL) {
-            strcpy(row[count], token);
+            strcpy(row, token);
         token = strtok(NULL, delimiter);
         count += 1;
     }
-    strcpy(row[count], token);
+    strcpy(row, token);
     }
     // Free the duplicated string
     free(inputCopy);
@@ -56,7 +56,7 @@ FILE *open_file(char *filename, char *mode)
  * @param dataArray the array of readings
  * @return int Returns the number of readings from the file
  */
-int read_file(FILE *inputFile, MAZE_ROW *DATA,MAZE_ROW *MEMORY)
+int read_file(FILE *inputFile, char output[100][100])
 {
 	//Initiation of variables
     int buffer_size = 100,len=0,function_count=0;
@@ -64,20 +64,9 @@ int read_file(FILE *inputFile, MAZE_ROW *DATA,MAZE_ROW *MEMORY)
     char line_buffer[buffer_size],delimiter = ',';
 
     while(fgets(line_buffer, buffer_size, inputFile) != NULL){
-        //Use the function provided
-        tokeniseRecord(line_buffer,&delimiter, DATA*->row);
-        //grab the length of Data Steps
-        len = strlen(DATA->steps)-1;
-        //See if the end of the string still has a format
-        if (DATA->steps && DATA->steps[len] == '\n') {
-            DATA->steps[len] = '\0';
-            }
-        //Copy the 3 first lines to print them later
-        
-        strcpy(MEMORY[function_count].date, DATA->date);
-        strcpy(MEMORY[function_count].time, DATA->time);
-        strcpy(MEMORY[function_count].steps, DATA->steps);
-            
+        //strcpy(output,line_buffer);
+        //grab the length of Data Steps   
+        strcpy(output[function_count],line_buffer);
         function_count++;
     }
 	return function_count;
@@ -90,78 +79,17 @@ int read_file(FILE *inputFile, MAZE_ROW *DATA,MAZE_ROW *MEMORY)
  * @param Count The number of readings in the array
  * @return int Return 0 if there are no errors, 1 if you find an error.
  */
-int data_checker(FITNESS_DATA *DATA, int count)
+int data_checker(MAZE_ROW *DATA, int count)
 {
 	if (count == 0){
 		return 1;
 	}
     for(int i = 0; i< count; i++){
-        if (DATA[i].steps == NULL){
+        if (DATA[i].row == NULL){
             return 1;
         }
     }
     return 0;
 }
-
-
-/**
- * @brief Calculates and returns the mean of the readings in the array
- * 
- * @param dataArray The array of data from the file
- * @param numReadings The number of readings in the array
- * @return float The mean of the readings.
- */
-float find_mean(FITNESS_DATA* dataArray, int numReadings)
-{
-    float mean = 0;
-    for (int i=0; i<numReadings;i++){
-        mean += atoi(dataArray[i].steps);
-    }
-    mean /= numReadings;
-    return mean;
-}
-
-/**
- * @brief Finds and returns the highest blood iron reading
- * 
- * @param dataArray The array of data from the file
- * @param numReadings The number of readings in the array
- * @return float The highest blood iron reading
- */
-int find_highest(FITNESS_DATA* DATA, int count)
-{
-    int maximun = atoi(DATA[0].steps);
-	int rank;
-    for(int i = 1; i < count;i++){
-        if (maximun < atoi(DATA[i].steps)){
-            maximun = atoi(DATA[i].steps);
-			rank=i;
-        }
-    }
-    printf("Largest steps: %s %s\n",DATA[rank].date,DATA[rank].time);
-    return maximun;
-}
-
-/**
- * @brief Finds and returns the lowest blood iron reading
- * 
- * @param dataArray The array of data from the file
- * @param numReadings The number of readings in the array
- * @return float The lowest blood iron reading
- */
-int find_lowest(FITNESS_DATA* DATA, int count)
-{
-	int minimun = atoi(DATA[0].steps);
-	int rank;
-    for(int i = 1; i < count;i++){
-        if (minimun > atoi(DATA[i].steps)){
-            minimun = atoi(DATA[i].steps);
-			rank=i;
-        }
-    }
-    printf("Fewest steps: %s %s\n",DATA[rank].date,DATA[rank].time);
-    return minimun;
-}
-
 
 // #endif // FITNESS_DATA_STRUCT_H
--- Maze game/maze.c
+++ Maze game/maze.c
@@ -1,1 +1,18 @@
 #include "MazeDataStruct.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+    
+    char *filename = "maze.csv",output[100][100];
+    FILE *file = open_file(filename,"r");
+    int count = read_file(file,output);
+    for (int i = 0; i < count; i++)
+    {
+        printf("%s",output[i]);
+    }
+    
+    
+}
