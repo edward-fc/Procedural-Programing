86163ac91fc0632e6306570d9e305918fa1ae142
Edward Carter <146713869+edward-fc@users.noreply.github.com>
2024-02-12 14:13:57 +0000

    finished the maze random generator

---
+++ Maze game/MazeDataStruct.h
@@ -0,0 +1,167 @@
+
+// #ifndef FITNESS_DATA_STRUCT_H
+// #define FITNESS_DATA_STRUCT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char row[100];
+} MAZE_ROW;
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *row[100]) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    delimiter=",";
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    int count = 0;
+    while (token != NULL){
+        
+        if (token != NULL) {
+            strcpy(row[count], token);
+        token = strtok(NULL, delimiter);
+        count += 1;
+    }
+    strcpy(row[count], token);
+    }
+    // Free the duplicated string
+    free(inputCopy);
+    }
+
+/**
+ * @brief Opens the file in the correct mode
+ *
+ * @param filename the name of the file to open
+ * @param mode the mode (r/w/a/r+/w+/a+)
+ * @return FILE* The file object to store the opened file in.
+ */
+FILE *open_file(char *filename, char *mode)
+{
+    FILE *input = fopen(filename, mode);
+    return input;
+}
+
+/**
+ * @brief Reads the data from the input file into an array of structs
+ *
+ * @param inputFile the open file object
+ * @param dataArray the array of readings
+ * @return int Returns the number of readings from the file
+ */
+int read_file(FILE *inputFile, MAZE_ROW *DATA,MAZE_ROW *MEMORY)
+{
+	//Initiation of variables
+    int buffer_size = 100,len=0,function_count=0;
+    
+    char line_buffer[buffer_size],delimiter = ',';
+
+    while(fgets(line_buffer, buffer_size, inputFile) != NULL){
+        //Use the function provided
+        tokeniseRecord(line_buffer,&delimiter, DATA*->row);
+        //grab the length of Data Steps
+        len = strlen(DATA->steps)-1;
+        //See if the end of the string still has a format
+        if (DATA->steps && DATA->steps[len] == '\n') {
+            DATA->steps[len] = '\0';
+            }
+        //Copy the 3 first lines to print them later
+        
+        strcpy(MEMORY[function_count].date, DATA->date);
+        strcpy(MEMORY[function_count].time, DATA->time);
+        strcpy(MEMORY[function_count].steps, DATA->steps);
+            
+        function_count++;
+    }
+	return function_count;
+}
+
+/**
+ * @brief Checks that there was data for each part of each reading in the file
+ *
+ * @param DATA The array of data from the file
+ * @param Count The number of readings in the array
+ * @return int Return 0 if there are no errors, 1 if you find an error.
+ */
+int data_checker(FITNESS_DATA *DATA, int count)
+{
+	if (count == 0){
+		return 1;
+	}
+    for(int i = 0; i< count; i++){
+        if (DATA[i].steps == NULL){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+/**
+ * @brief Calculates and returns the mean of the readings in the array
+ * 
+ * @param dataArray The array of data from the file
+ * @param numReadings The number of readings in the array
+ * @return float The mean of the readings.
+ */
+float find_mean(FITNESS_DATA* dataArray, int numReadings)
+{
+    float mean = 0;
+    for (int i=0; i<numReadings;i++){
+        mean += atoi(dataArray[i].steps);
+    }
+    mean /= numReadings;
+    return mean;
+}
+
+/**
+ * @brief Finds and returns the highest blood iron reading
+ * 
+ * @param dataArray The array of data from the file
+ * @param numReadings The number of readings in the array
+ * @return float The highest blood iron reading
+ */
+int find_highest(FITNESS_DATA* DATA, int count)
+{
+    int maximun = atoi(DATA[0].steps);
+	int rank;
+    for(int i = 1; i < count;i++){
+        if (maximun < atoi(DATA[i].steps)){
+            maximun = atoi(DATA[i].steps);
+			rank=i;
+        }
+    }
+    printf("Largest steps: %s %s\n",DATA[rank].date,DATA[rank].time);
+    return maximun;
+}
+
+/**
+ * @brief Finds and returns the lowest blood iron reading
+ * 
+ * @param dataArray The array of data from the file
+ * @param numReadings The number of readings in the array
+ * @return float The lowest blood iron reading
+ */
+int find_lowest(FITNESS_DATA* DATA, int count)
+{
+	int minimun = atoi(DATA[0].steps);
+	int rank;
+    for(int i = 1; i < count;i++){
+        if (minimun > atoi(DATA[i].steps)){
+            minimun = atoi(DATA[i].steps);
+			rank=i;
+        }
+    }
+    printf("Fewest steps: %s %s\n",DATA[rank].date,DATA[rank].time);
+    return minimun;
+}
+
+
+// #endif // FITNESS_DATA_STRUCT_H
---
+++ Maze game/create.py
@@ -0,0 +1,69 @@
+#maze list
+import random
+maze=[]
+list_item = ["#"," ","S","E"]
+maze_row_index = 10
+maze_collum_index = 30
+for i in range(maze_row_index):
+    row=[]
+    for y in range(maze_collum_index):
+        row.append("#")
+    maze.append(row)
+
+start_point = random.randint(1,maze_collum_index)
+state = True     
+square_index = [1,start_point]
+while state:
+    #make condition to randomly make a maze that conects
+    if random.randint(0,1):
+        #lets try and go left
+        if square_index[1]-1 != 0:
+            maze[square_index[0]][square_index[1]] = " "
+            square_index[1] -= 1
+
+    else:
+        #lets try and go right
+        if square_index[1]+1 != maze_collum_index:
+            maze[square_index[0]][square_index[1]] = " "
+            square_index[1] += 1
+
+    if random.randint(0,1):
+        #lets try and go down
+        if square_index[0]+1 != maze_row_index:
+            maze[square_index[0]][square_index[1]] = " "
+            square_index[0] += 1
+    """    
+    else:
+        #lts try and go up
+        if square_index[1]+1 != maze_collum_index:
+            maze[square_index[0]][square_index[1]] = " "
+            square_index[1] += 1
+    """
+    if square_index[0]+1 == maze_row_index:
+        state = False
+end_point = list_item[random.randint(2,3)]
+start_point_text = "S"
+if end_point == "S":
+    start_point_text = "E"
+
+
+maze[1][start_point] = start_point_text
+maze[square_index[0]][square_index[1]] = end_point
+
+
+ 
+
+        
+
+
+
+
+
+f = open("Maze game/maze.csv", "w")
+s=""
+for element in maze:
+    for squares in element:
+        s += squares+","
+    s+="\n"
+f.write(s)
+f.close()
